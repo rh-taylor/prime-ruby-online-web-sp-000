e6f2eb5a52e10d6a0614c8cfe0c9e0c3aff48dfd
Robyn Taylor <rhtaylor0715@gmail.com>
2021-04-14 18:10:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+  num_array = (2..num).to_a
+  num_array.each do |int|
+    if int / 2 == 0 || int / 3 == 0
+      return false
+    else int / 2 != 0 || int / 3 != 0
+      return true
+    end
+  end
+end
